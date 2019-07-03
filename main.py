6fa239e5341b4aba16d54eaf0a7c7387e5985dfd
brandon <btmbaseball99@gmail.com>
2019-07-03 12:34:36 -0700

    layout setup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+from PyQt5.QtCore import QRect
 from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
         QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
         QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
@@ -21,20 +21,55 @@ class Main(QWidget):
         self.width = 2100
         self.height = 1400
 
+
+        self.createGraph()
+        self.createMetrics()
+        self.layout()
         self.setWindowTitle(self.title)
         self.setGeometry(self.x, self.y, self.width, self.height)
         self.show()
 
     def layout(self):
-        header = QHBoxLayout()
-        name = QLabel("crypto b0t")
-        header.addWidget(name)
-        header.addStretch(2)
+        logo = QLabel("Crypto B0t")
+        sign_in_btn = QPushButton("Sign In")
+        sign_in_btn.setGeometry(QRect(0, 0, 50, 100))
+        sign_up_btn = QPushButton("Sign Up")
+        topLayout = QHBoxLayout()
+        topLayout.addWidget(logo)
+        topLayout.addWidget(sign_in_btn)
+        topLayout.addWidget(sign_up_btn)
 
         main = QGridLayout()
-
+        main.addLayout(topLayout, 0, 0, 1, 2)
+        main.addWidget(self.graphBox, 1, 0)
+        main.addWidget(self.metricsBox, 1, 1)
         self.setLayout(main)
 
+    def createGraph(self):
+        self.graphBox = QGroupBox("Graph")
+        graph_label = QLabel("Graphs")
+        graph_label.setGeometry(QRect(0, 0, 100, 700))
+        middleLeft = QVBoxLayout()
+        middleLeft.setContentsMargins(50, 50, 50, 50)
+        middleLeft.addWidget(graph_label)
+        middleLeft.addStretch(1)
+        self.graphBox.setLayout(middleLeft)
+        
+    
+    def createMetrics(self):
+        self.metricsBox = QGroupBox("Metrics")
+
+        transaction_log = QLabel("Transaction Log")
+        transaction_log.setGeometry(QRect(0, 0, 400, 700))
+        middleRight = QVBoxLayout()
+        middleRight.addWidget(transaction_log)
+        self.graphBox.setLayout(middleRight)
+
+    def settings(self): 
+        pass
+
+    def footer(self):
+        pass
 
 
     

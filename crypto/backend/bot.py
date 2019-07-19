df4239d2c246df9a94fdc23592d2c8c036a675c3
Brandon Mitchell <brandon@feasible.io>
2019-07-19 09:12:03 -0700

    get_current_data update, gathers current price and volume

--- crypto/backend/bot.py
+++ crypto/backend/bot.py
@@ -3,7 +3,16 @@ import time
 import json
 import pandas as pd
 
+# TODO:
+#   FILTERS THE DATA MUST PASS BEFORE BUYING:
+#       1. There is a negative to positive slope at time of buy
+#       2. Buying will be below the previous sold price
+#       3. Price will be below weekly average       !!!! Nearly Done !!!!
 
+#   FILTERS THE DATA MUST PASS BEFORE SELLING:
+#       1. There is a positive to negative slope at time of sale
+#       2. Price is above bought price (>1%)
+#       3. Price will be above weekly average       !!!! Nearly Done !!!!
 
 class Bot:
 
@@ -30,6 +39,50 @@ class Bot:
         self.exchange.fetch_balance()    
         self.exchange.fetch_trades('USDC/BTC')
 
+    def boughtPrice(self):
+        '''
+        hold variable for initial buy price
+        '''
+        pass
+
+    def weekly_average(self, coin):
+        '''
+        gathers market data, displays a dataframe including date, open, high, low, close, and volume.
+        from this we gather the weekly price average (a filter for buying and selling)
+        '''
+        self.balance = self.exchange.fetch_balance()
+
+        # iterate through tickers related to the coin and construct dataframe around data
+        for ticker in self.exchange.fetch_tickers(coin):
+            time.sleep(self.exchange.rateLimit / 1000)
+            resp = self.exchange.fetch_ohlcv(ticker, '1m')
+            df = pd.DataFrame(resp, columns=['date', 'open', 'high', 'low', 'close', 'volume'])
+            df.style.hide_index()
+            print(df)
+
+            week_avg = df.tail(7)['close'].mean()
+            self.get_current_data(df)
+
+        # for trade in exchange.fetch_trades('USDC/BTC', since=):
+        #     if trade['side'] == 'buy':
+        #         print(trade['price'])
+        #         print(trade['amount'])
+
+    def get_current_data(self, data):
+        '''
+        gathers current data
+        :type: coin --> string referencing specific crypto i.e. 'btc'
+        :rtype: current_price --> int 
+        :rtype: current_vol --> int
+        :rtype: 
+        '''
+        current_price = float(str(data.tail(1)['close'])[7:15])
+        current_vol = float(str(data.tail(1)['volume'])[7:15])
+        current_supp = ''
+        print(data.tail(1)['close'])
+        print(f'Current Price: {current_price}')
+        print(f'Current Volumne: {current_vol}')
+
 # print(exchange.load_markets)
 # print(exchange.symbols)
 # orders = exchange.fetch_order_book(exchange.symbols[0])
@@ -40,6 +93,9 @@ class Bot:
 # # print(binance.fetch_ticker('BTC/USDC')['weightedAvgPrice'])
 
 
+
+
+# EXAMPLE ON MARKET ORDER
 
 # symbol = 'XRP/BTC'
 # type = 'market'
@@ -53,60 +109,9 @@ class Bot:
 # order = exchange.create_order(symbol, type, side, amount)
 # print(order)
 
-# TODO:
-#   FILTERS THE DATA MUST PASS BEFORE BUYING:
-#       1. There is a negative to positive slope at time of buy
-#       2. Buying will be below the previous sold price
-#       3. Price will be below weekly average       !!!! Nearly Done !!!!
 
-#   FILTERS THE DATA MUST PASS BEFORE SELLING:
-#       1. There is a positive to negative slope at time of sale
-#       2. Price is above bought price (>1%)
-#       3. Price will be above weekly average       !!!! Nearly Done !!!!
-
-
-    def boughtPrice(self):
-        '''
-        hold variable for initial buy price
-        '''
-        pass
-
-    def weeklyAvg(self, coin):
-        '''
-        gathers market data, displays a dataframe including date, open, high, low, close, and volume.
-        from this we gather the weekly price average (a filter for buying and selling)
-        '''
-        self.balance = self.exchange.fetch_balance()
-
-        # iterate through tickers related to the coin and construct dataframe around data
-        for ticker in self.exchange.fetch_tickers(coin):
-            time.sleep(self.exchange.rateLimit / 1000)
-            resp = self.exchange.fetch_ohlcv(ticker, '1d')
-            df = pd.DataFrame(resp, columns=['date', 'open', 'high', 'low', 'close', 'volume'])
-            print(df)
-
-            week_avg = df.tail(7)['close'].mean()
-            print(week_avg)
-
-        # for trade in exchange.fetch_trades('USDC/BTC', since=):
-        #     if trade['side'] == 'buy':
-        #         print(trade['price'])
-        #         print(trade['amount'])
-
-    def get_current_data(self, coin):
-        '''
-        gathers current data
-        :type: coin --> string referencing specific crypto i.e. 'btc'
-        :rtype: current_price --> int 
-        :rtype: current_vol --> int
-        :rtype: 
-        '''
-
-        # current_price = resp['USD']['rate']
-        current_mcap = ''
-        current_supp = ''
 
 if __name__ == '__main__':
     bot = Bot()
-    bot.weeklyAvg('BTC/USDC')
+    bot.weekly_average('BTC/USDC')
     print('done')
